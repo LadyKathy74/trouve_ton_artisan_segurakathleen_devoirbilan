8619d328ffa3bd12823a964cd00f77f493baf3cf
LadyKathy <sakurasan74@yahoo.com>
2025-12-27 08:04:18 +0100

    first commit

---
+++ client/src/assets/data/artisansData.js
@@ -0,0 +1,226 @@
+import img1 from "../images/artisan1.jpg"; 
+import img2 from "../images/artisan2.jpg";
+import img3 from "../images/artisan3.jpg";
+import img4 from "../images/artisan4.jpg";
+import img5 from "../images/artisan5.jpg";
+import img6 from "../images/artisan6.jpg";
+import img7 from "../images/artisan7.jpg";
+import img8 from "../images/artisan8.jpg";
+import img9 from "../images/artisan9.jpg";
+import img10 from "../images/artisan10.jpg";
+import img11 from "../images/artisan11.jpg";
+import img12 from "../images/artisan12.jpg";
+import img13 from "../images/artisan13.jpg";
+import img14 from "../images/artisan14.jpg";
+import img15 from "../images/artisan15.jpg";
+import img16 from "../images/artisan16.jpg";
+import img17 from "../images/artisan17.jpg";
+
+
+
+export const artisansData = [
+  {
+    id: "1",
+    name: "Boucherie Dumont",
+    specialty: "Boucher",
+    rating: 4.5,
+    location: "Lyon",
+    description: "Des viandes sélectionnées avec exigence, préparées avec passion et respect des méthodes authentiques. Découvrez la qualité et le goût inégalé de nos produits frais.",
+    email: "boucherie.dumond@gmail.com",
+    website: "",
+    category: "Alimentation",
+    image: img1
+  },
+  {
+    id: "2",
+    name: "Au pain chaud",
+    specialty: "Boulanger",
+    rating: 4.8,
+    location: "Montélimar",
+    description: "Pain chaud, croissants dorés, savoir-faire artisanal. Venez savourer nos délices fraîchement sortis du four, préparés avec amour et tradition pour régaler vos papilles.",
+    email: "aupainchaud@hotmail.com",
+    website: "",
+    category: "Alimentation",
+    image: img2
+  },
+  {
+    id: "3",
+    name: "Chocolaterie Labbé",
+    specialty: "Chocolatier",
+    rating: 4.9,
+    location: "Lyon",
+    description: "Chocolats fins, passion pure, plaisir garanti. Découvrez nos créations artisanales, alliant tradition et innovation pour une expérience gustative inoubliable.",
+    email: "chocolaterie-labbe@gmail.com",
+    website: "https://chocolaterie-labbe.fr",
+    category: "Alimentation",
+    image: img3
+  },
+  {
+    id: "4",
+    name: "Traiteur Truchon",
+    specialty: "Traiteur",
+    rating: 4.1,
+    location: "Lyon",
+    description: "Des menus sur mesure, élaborés avec des produits frais et un savoir‑faire culinaire authentique. Faites de chaque événement un moment inoubliable avec notre service traiteur d'exception.",
+    email: "contact@truchon-traiteur.fr",
+    website: "https://truchon-traiteur.fr",
+    category: "Alimentation",
+    image: img4
+  },
+  {
+    id: "5",
+    name: "Orville Salmons",
+    specialty: "Chauffagiste",
+    rating: 5,
+    location: "Evian",
+    description: "Installation, entretien, dépannage : chaleur maîtrisée. Expertise et service de qualité pour votre confort thermique toute l'année.",
+    email: "o-salmons@live.com",
+    website: "",
+    category: "Bâtiment",
+    image: img5
+  },
+  {
+    id: "6",
+    name: "Mont Blanc Électricité",
+    specialty: "Electricien",
+    rating: 4.5,
+    location: "Chamonix",
+    description: "Courant maîtrisé, sécurité assurée, service rapide. Votre électricien de confiance pour tous vos besoins résidentiels et commerciaux.",
+    email: "contact@mont-blanc-electricite.com",
+    website: "https://mont-blanc-electricite.com",
+    category: "Bâtiment",
+    image: img6
+  },
+  {
+    id: "7",
+    name: "Boutot & fils",
+    specialty: "Menuisier",
+    rating: 4.7,
+    location: "Bourg-en-bresse",
+    description: "Bois travaillé avec passion, créations sur mesure. Donnez vie à vos projets avec notre expertise en menuiserie artisanale.",
+    email: "boutot-menuiserie@gmail.com",
+    website: "https://boutot-menuiserie.com",
+    category: "Bâtiment",
+    image: img7
+  },
+  {
+    id: "8",
+    name: "Vallis Bellemare",
+    specialty: "Plombier",
+    rating: 4,
+    location: "Vienne",
+    description: "Interventions rapides, solutions durables. Votre plombier de confiance pour tous vos besoins en installation, réparation et entretien sanitaire.",
+    email: "v.bellemare@gmail.com",
+    website: "https://plomberie-bellemare.com",
+    category: "Bâtiment",
+    image: img8
+  },
+  {
+    id: "9",
+    name: "Claude Quinn",
+    specialty: "Bijoutier",
+    rating: 4.2,
+    location: "Aix-les-bains",
+    description: "Bijoux uniques, savoir-faire exceptionnel. Découvrez nos créations artisanales qui allient élégance et qualité pour sublimer chaque moment de votre vie.",
+    email: "claude.quinn@gmail.com",
+    website: "",
+    category: "Fabrication",
+    image: img9
+  },
+  {
+    id: "10",
+    name: "Amitee Lécuyer",
+    specialty: "Couturier",
+    rating: 4.5,
+    location: "Annecy",
+    description: "Créations sur mesure, expertise en couture artisanale. Votre couturier de confiance pour des vêtements uniques et des retouches parfaites.",
+    email: "a.amitee@hotmail.com",
+    website: "https://lecuyer-couture.com",
+    category: "Fabrication",
+    image: img10
+  },
+  {
+    id: "11",
+    name: "Ernest Carignan",
+    specialty: "Ferronier",
+    rating: 5,
+    location: "Le Puy-en-Velay",
+    description: "Ferronnerie artisanale, expertise et créations sur mesure. Votre ferronier de confiance pour tous vos projets métalliques.",
+    email: "e-carigan@hotmail.com",
+    website: "",
+    category: "Fabrication",
+    image: img11
+  },
+  {
+    id: "12",
+    name: "Royden Charbonneau",
+    specialty: "Coiffeur",
+    rating: 3.8,
+    location: "Saint-Priest",
+    description: "Style, soin, et coup de maître. Votre coiffeur de confiance pour un look impeccable.",
+    email: "r.charbonneau@gmail.com",
+    website: "",
+    category: "Services",
+    image: img12
+  },
+  {
+    id: "13",
+    name: "Leala Dennis",
+    specialty: "Coiffeur",
+    rating: 3.8,
+    location: "Chambéry",
+    description: "Style, soin, et coup de maître. Votre coiffeur de confiance pour un look impeccable.",
+    email: "l.dennos@hotmail.fr",
+    website: "https://coiffure-leala-chambery.fr",
+    category: "Services",
+    image: img13
+  },
+  {
+    id: "14",
+    name: "C'est sup'hair",
+    specialty: "Coiffeur",
+    rating: 4.1,
+    location: "Romans-sur-Isère",
+    description: "Style, soin, et coup de maître. Votre coiffeur de confiance pour un look impeccable.",
+    email: "sup-hair@gmail.com",
+    website: "https://sup-hair.fr",
+    category: "Services",
+    image: img14
+  },
+  {
+    id: "15",
+    name: "Le monde des fleurs",
+    specialty: "Fleuriste",
+    rating: 4.6,
+    location: "Annonay",
+    description: "Bouquets sur mesure, fraîcheur et poésie. Votre fleuriste de confiance pour toutes les occasions.",
+    email: "contact@le-monde-des-fleurs-annonay.fr",
+    website: "https://le-monde-des-fleurs-annonay.fr",
+    category: "Services",
+    image: img15
+  },
+  {
+    id: "16",
+    name: "Valérie Laderoute",
+    specialty: "Toiletteur",
+    rating: 4.5,
+    location: "Valence",
+    description: "Soins et beauté pour vos compagnons. Votre toiletteur de confiance pour un service attentionné et professionnel.",
+    email: "v-laredoute@gmail.com",
+    website: "",
+    category: "Services",
+    image: img16
+  },
+  {
+    id: "17",
+    name: "CM Graphisme",
+    specialty: "Webdesign",
+    rating: 4.4,
+    location: "Valence",
+    description: "Design web et graphisme sur mesure. Votre graphiste de confiance pour un site web moderne et attrayant.",
+    email: "contact@cm-graphisme.com",
+    website: "https://cm-graphisme.com",
+    category: "Services",
+    image: img17
+  }
+];
